19f0a2540f00dafc89b8d72679480533eb152237
Samuel-dot-cloud <successam336@gmail.com>
2020-11-22 15:21:05 +0300

    Completing GUI basics with texfield, radiobutton, checkbox and spinner

--- app/src/main/java/com/manasseh/gui/MainActivity.java
+++ app/src/main/java/com/manasseh/gui/MainActivity.java
@@ -2,20 +2,30 @@ package com.manasseh.gui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    @BindView(R.id.name)
+    EditText nameSpace;
     String[] foods = { "Egg curry", "Meat stew", "Ugali", "Rice", "Bean stew" };
+    private String food, name, gender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         Spinner spin = (Spinner) findViewById(R.id.spinner1);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, foods);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -31,7 +41,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         switch(view.getId()) {
             case R.id.chk1:
                 if (checked){
-
+                    name = nameSpace.getText().toString();
+                    Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                    intent.putExtra("name", String.valueOf(name));
+                    intent.putExtra("food", String.valueOf(food));
+                    intent.putExtra("gender", String.valueOf(gender));
+                    startActivity(intent);
                 }
                 // Do your coding
         else
@@ -44,11 +59,34 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+        // Check which Checkbox was clicked
+        gender = "";
+        switch(view.getId()) {
+            case R.id.chkMale:
+                if (checked) {
+                    gender = checked?"Male Selected":"Male Deselected";
+                }
+
+                break;
+
+            case R.id.chkFemale:
+                if (checked) {
+                    gender = checked?"Female Selected":"Female Deselected";
+                }
+
+                break;
+
+            default:
+        }
+        Toast.makeText(getApplicationContext(), gender, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getApplicationContext(), "Selected User: "+foods[position] , Toast.LENGTH_SHORT).show();
+        food = foods[position];
+        Toast.makeText(getApplicationContext(), "Selected food: "+food , Toast.LENGTH_SHORT).show();
     }
 
     @Override
--- app/src/main/java/com/manasseh/gui/ResultActivity.java
+++ app/src/main/java/com/manasseh/gui/ResultActivity.java
@@ -3,12 +3,32 @@ package com.manasseh.gui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ResultActivity extends AppCompatActivity {
+    @BindView(R.id.nameSpace)
+    TextView nameView;
+    @BindView(R.id.genderSpace)
+    TextView genderView;
+    @BindView(R.id.foodSpace)
+    TextView foodView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        ButterKnife.bind(this);
+
+        String name = getIntent().getStringExtra("name");
+        String food = getIntent().getStringExtra("food");
+        String gender = getIntent().getStringExtra("gender");
+
+
+        nameView.setText(name);
+        genderView.setText(gender);
+        foodView.setText(food);
     }
 }
